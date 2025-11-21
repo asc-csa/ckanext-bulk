ffa89c4b7c622bfe0dded3cfda5d9acedb18e185
Tyler Kennedy <tk@tkte.ch>
2025-11-21 12:36:53 -0500

    English and French

--- ckanext/bulk/plugin.py
+++ ckanext/bulk/plugin.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 from ckan.common import CKANConfig
@@ -12,9 +14,20 @@ from ckan.common import CKANConfig
 @tk.blanket.validators
 class BulkPlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer)
+    p.implements(p.ITranslation)
 
     # IConfigurer
     def update_config(self, config_: CKANConfig):
         tk.add_template_directory(config_, "templates")
         tk.add_public_directory(config_, "public")
         tk.add_resource("assets", "bulk")
+
+    # ITranslation
+    def i18n_directory(self):
+        return os.path.join(os.path.dirname(__file__), "i18n")
+
+    def i18n_locales(self):
+        return ["fr"]
+
+    def i18n_domain(self):
+        return "ckanext-bulk"
